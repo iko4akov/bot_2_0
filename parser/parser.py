3b61682a32bcf06e0714423ffde0d5a4986cba13
iko4akov <iko4akov@gmail.com>
2025-03-14 15:59:01 +0300

    complete resend

--- parser/parser.py
+++ parser/parser.py
@@ -1,11 +1,9 @@
+import base64
 from typing import Optional
 
-from aiogram import types
-from aiogram.types import InputFile
 from telethon import TelegramClient, events
 from telethon.tl.types import MessageMediaPhoto, MessageMediaDocument, MessageMediaDocument, DocumentAttributeVideo, Message
 
-from bot import bot
 from database.models import Users
 from utils import logger
 
@@ -20,19 +18,10 @@ async def start_monitoring(client: Optional[TelegramClient], user: Users):
 async def forward_message(message: Message, target_channel, client: Optional[TelegramClient]):
     try:
         if message.message:
-            await client.send_message(target_channel, message.message)
+            await client.send_message(entity=target_channel, message=message.message)
 
-        elif message.media and isinstance(message.media, MessageMediaPhoto):
-            await client.send_file(entity=target_channel, file=message.media.photo.file_reference)
-
-        elif message.media and isinstance(message.media, MessageMediaDocument):
-            for attr in message.media.document.attributes:
-                if isinstance(attr, DocumentAttributeVideo):
-                    await client.send_file(
-                        entity=target_channel,
-                        file=message.media.document.file_reference
-                    )
-                    break
+        elif message.media:
+            await client.send_file(entity=target_channel, file=message.media)
 
     except Exception as e:
-        print(f"Ошибка при отправке сообщения: {e}")
+        logger.error(f"Ошибка при отправке сообщения: {e}")
