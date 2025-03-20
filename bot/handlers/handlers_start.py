a335ed022870b5e28301bb7664ecd0bf5410035a
iko4akov <iko4akov@gmail.com>
2025-03-20 13:24:35 +0300

    ref

--- bot/handlers/handlers_start.py
+++ bot/handlers/handlers_start.py
@@ -6,6 +6,7 @@ from database.models import Users
 from bot.constants.constans import INFO_MESSAGE
 from bot.keyboard import kb
 from database.services.crud_user import create_user, get_user
+from utils import logger
 
 router = Router()
 
@@ -15,10 +16,17 @@ async def command_start_handler(message: Message) -> None:
     """
     Просто выкидывает меню команда: /start
     """
+    logger.info(f"User started the bot: {message.from_user.id}")
+
     new_user = Users.from_message(message)
     check_user = await get_user(message.from_user.id)
     if not check_user:
-        await create_user(new_user)
+        try:
+            await create_user(new_user)
+        except Exception as e:
+            logger.error(f"Ошибка создания пользователя: {e}")
+            await message.answer("Произошла ошибка при регистрации пользователя.")
+
     await message.answer(INFO_MESSAGE, reply_markup=kb.get_inline_markup())
 
 @router.message(lambda m: m.text == 'Меню')
@@ -26,4 +34,5 @@ async def menu(message: Message):
     """
     Просто выкидывает меню команда: Меню
     """
+    logger.info(f"Пользователь: {message.from_user.id} вызвал меню")
     await message.answer(INFO_MESSAGE, reply_markup=kb.get_inline_markup())
